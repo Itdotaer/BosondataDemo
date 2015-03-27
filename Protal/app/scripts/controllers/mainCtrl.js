1af1a7bf13d16e4ada9fc614f6b1d07aa784146e
SoftwareDreamer <hujiangtao1235@gmail.com>
2015-03-27 10:40:05 +0800

    延时数据加载，for看到加载效果。

--- Protal/app/scripts/controllers/mainCtrl.js
+++ Protal/app/scripts/controllers/mainCtrl.js
@@ -14,12 +14,11 @@
         .controller('mainController', mainController);
 
     //Inject modules
-    mainController.$inject = ['analysisService', 'logger', 'DEBUG'];
+    mainController.$inject = ['analysisService', 'logger', 'DEBUG', '$interval'];
 
-    function mainController(analysisService, logger, DEBUG) {
+    function mainController(analysisService, logger, DEBUG, $interval) {
         var vm = this;
         vm.text = '15日，备受关注的电影《黄金时代》在北京举行了电影发布会，导演许鞍华和编剧李樯及汤唯、冯绍峰等众星悉数亮相。据悉，电影确定将于10月1日公映。本片讲述了“民国四大才女”之一的萧红短暂而传奇的一生，通过她与萧军、汪恩甲、端木蕻良、洛宾基四人的情感纠葛，与鲁迅、丁玲等人一起再现上世纪30年代的独特风貌。电影原名《穿过爱情的漫长旅程》，后更名《黄金时代》，这源自萧红写给萧军信中的一句话：“这不正是我的黄金时代吗？”';
-        vm.isLoading = true;
 
         vm.identifyTagName = identifyTagName;
         vm.tagClass = tagClass;
@@ -32,23 +31,25 @@
         }
 
         function requestAnalysis() {
+            vm.isLoading = true;
+
             if (vm.text == "" && vm.text != undefined) {
                 logger.logError('输入文本为空!');
             } else {
                 //Use angular $http
-                analysisService.requestAnalysis(vm.text)
-                    .then(function (data) {
-                        if (DEBUG) {
-                            logger.logInfo('成功，词性分析成功!');
-                        }
+                //analysisService.requestAnalysis(vm.text)
+                //    .then(function (data) {
+                //        if (DEBUG) {
+                //            logger.logInfo('成功，词性分析成功!');
+                //        }
 
-                        vm.data = data;
-                        vm.isLoading = false;
-                    })
-                    .catch(function (e) {
-                        vm.isLoading = false;
-                        logger.logError('请求分析出错!');
-                    });
+                //        vm.data = data;
+                //        vm.isLoading = false;
+                //    })
+                //    .catch(function (e) {
+                //        vm.isLoading = false;
+                //        logger.logError('请求分析出错!');
+                //    });
 
                 //Use ajax post
                 //analysisService.requestAnalysisUseAjax(vm.text, function (err, data) {
@@ -67,8 +68,11 @@
             }
 
             //Mock service
-            //vm.data = analysisService.mockRequestAnalysis();
-            //vm.isLoading = false;
+            vm.data = analysisService.mockRequestAnalysis();
+            //Mock 速度太快， 为了让你看到效果，我加了一个延时。
+            $interval(function() {
+                vm.isLoading = false;
+            }, 3000);
         }
 
         //词性标注是按照已有页面匹配，数据不全，如有不准，仅供参考。
